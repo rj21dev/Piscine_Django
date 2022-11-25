83097a27f9acb65f4502135e114c68b176b0ce80
Oleg Fomin <oleg.fomin369@yandex.ru>
2022-11-25 14:58:19 +0300

    Add: ex02

--- Starting/ex01/numbers.py
+++ Starting/ex01/numbers.py
@@ -1,7 +1,11 @@
-if __name__ == '__main__':
-	with open("numbers.txt", 'r') as file:
+def print_numbers(filename):
+	with open(filename, 'r') as file:
 		for line in file:
 			line = line[:-1]
 			elems = line.split(',')
 		for elem in elems:
 			print(elem)
+
+
+if __name__ == '__main__':
+	print_numbers("numbers.txt")
